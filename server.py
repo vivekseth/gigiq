5a18747949271a98d93d0595f17d56a316bbb00b
Vivek Seth <Vivekseth.m@gmail.com>
2016-01-31 17:21:37 -0500

    added jobs search endpoint

--- server.py
+++ server.py
@@ -1,24 +1,34 @@
 import sys
 import signal
+
 from bson import json_util
 from datetime import datetime
-from flask import Flask, request
+from haversine import haversine
+
+from pymongo import GEOSPHERE
 from pymongo import MongoClient
+from flask import Flask, request
 
 app = Flask(__name__)
 client = MongoClient()
 db = client['gigiqdb']
 jobs_col = db['jobs']
 
+jobs_col.create_index([('pickup', GEOSPHERE)])
+jobs_col.create_index([('dropoff', GEOSPHERE)])
+
 def signal_handler(signal, frame):
 	print '\nclosing database...'
 	client.close()
 	sys.exit(0)
 
+def row_array(cursor):
+	return [j for j in cursor]
+
 def validate_latlng(latlng):
-	if not 0.0 < float(latlng['lat']) < 360.0:
+	if not -90.0 < float(latlng['lat']) < 90.0:
 		return False
-	elif not 0.0 < float(latlng['lng']) < 360.0:
+	elif not -180.0 < float(latlng['lng']) < 180.0:
 		return False
 	else:
 		return True
@@ -28,15 +38,34 @@ def validate_accept_job(job_obj):
 		if len(job_obj['service']) == 0:
 			raise ValueError('Invalid service name')
 		
-		if not validate_latlng(json_util.loads(str(job_obj['pickup']))):
+		if not validate_latlng(job_obj['pickup']):
 			raise ValueError('Invalid pickup location')
 		
-		if not validate_latlng(json_util.loads(str(job_obj['dropoff']))):
+		if not validate_latlng(job_obj['dropoff']):
 			raise ValueError('Invalid dropoff location')		
 		
 		return True
 	except Exception, e:
 		print e
+
+def job_cost_factory(lat, lng):
+	pos = (lat, lng)
+	travel_cost = -0.3
+	delivery_cost = 1.0
+	def cost(job):
+		pickup_tuple = (job['pickup']['lat'], job['pickup']['lng'])
+		dropoff_tuple = (job['dropoff']['lat'], job['dropoff']['lng'])
+
+		pickup_dist = haversine(pos, pickup_tuple)
+		delivery_dist = haversine(pickup_tuple, dropoff_tuple)
+
+		total_comp = travel_cost * (pickup_dist + delivery_dist) + delivery_cost * (delivery_dist)
+		return total_comp / (pickup_dist + delivery_dist)
+
+	return cost
+
+
+
 
 @app.route('/')
 def index():
@@ -48,7 +77,6 @@ def create_job():
 	if validate_accept_job(job_obj):
 		job_obj['timestamp'] = datetime.utcnow()
 		jobs_col.insert_one(job_obj)
-		print str(job_obj)
 		return 'success'
 	else:
 		return 'invalid data'
@@ -67,6 +95,19 @@ def delete_jobs():
 def accept_job(job_id):
 	return 'will accept job: {}.'.format(job_id)
 
+
+def near_find_options(lat, lng, min_dist, max_dist):
+	return {
+		'$near': {
+			'$geometry': {
+				'type': "Point" ,
+				'coordinates': [lat , lng]
+			},
+			'$maxDistance': max_dist,
+			'$minDistance': min_dist
+		}
+	}
+
 # Heartbeat
 @app.route('/api/jobs/search', methods=['GET'])
 def search_jobs():
@@ -79,7 +120,13 @@ def search_jobs():
 	try:
 		lat = float(lat_str)
 		lng = float(lng_str)
-		return 'searching from ({}, {}) for {}'.format(lat, lng, services)
+
+		jobs = row_array(jobs_col.find(limit=10, filter={
+			'pickup': near_find_options(lat, lng, 0, 100000)
+		}))
+		sorted_jobs = sorted(jobs, key=job_cost_factory(lat, lng))
+
+		return json_util.dumps(sorted_jobs)
 	except ValueError, e:
 		return 'invalid lat/lng'
 
